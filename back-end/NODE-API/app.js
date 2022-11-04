1e6a95ad06eef86047dd3bb63d1afad0fcd63996
Lorenzobenitez87 <105927927+Lorenzobenitez87@users.noreply.github.com>
2022-11-04 19:50:35 +0100

    création serveur

---
+++ back-end/NODE-API/app.js
@@ -0,0 +1,55 @@
+// On appel le framework express
+const express = require('express');
+const app = express();
+
+// On appel mongoose 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv:https://data.mongodb-api.com/app/data-ygfmu/endpoint/data/v1:<password>nFr9mK.JV!h9kxc',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// Express prend toutes les requêtes qui ont comme Content-Type application/json et met à disposition leur body directement sur l'objet req
+app.use(express.json());
+
+// 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'Objet crée !'
+    });
+});
+
+app.get('/api/stuff', (req, res, next) => {
+    const stuff = [
+        {
+            _id: 'oeihfzeoi',
+            title: 'Mon premier objet',
+            description: 'Les infos de mon premier objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 4900,
+            userId: 'qsomihvqios',
+        },
+        {
+            _id: 'oeihfzeomoihi',
+            title: 'Mon deuxième objet',
+            description: 'Les infos de mon deuxième objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 2900,
+            userId: 'qsomihvqios',
+        },
+    ];
+    res.status(200).json(stuff);
+});
+
+// Permet l'acces depuis les autres fichiers
+module.exports = app;
---
+++ back-end/NODE-API/server.js
@@ -0,0 +1,68 @@
+const http = require('http');
+const app = require('./app')
+
+// Fonction renvoie un port valide qu'il soit fourni sous forme de numéro ou de chaîne de caractère
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+};
+
+const port = normalizePort(process.env.PORT || '3000');
+
+app.set('port', port);
+
+// Fonction qui recherche les différentes erreurs et les gère de manière appropriée puis l'enregistre sur le server
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+
+
+//un écouteur d'évènements est également enregistré, consignant le port ou le canal nommé sur lequel le serveur s'exécute dans la console.
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port);
+
+
+// On appel le framework dotenv
+const dotenv = require("dotenv");
+dotenv.config();
+
+const MY_PORT = process.env.PORT;
+const MY_APP_SECRET = process.env.APP_SECRET;
+
+app.get("/", (req, res) => {
+    return res.send(MY_APP_SECRET);
+});
+
+app.listen(MY_PORT, () => console.log('server started: 3000'));
